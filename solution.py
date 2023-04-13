ca3e95a9d753697889bdc3b1919609b95f75b14c
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-13 12:48:56 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+import scipy.stats as stats
 
-chat_id = 1251251937 # Ваш chat ID, не меняйте название переменной
+chat_id = 1251251937
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
+             leads_test: int) -> bool:
+    control_failures = x_cnt - x_success
+    test_failures =  leads_test - y_success
+    table = [[x_success, control_failures], [y_success, test_failures]]
+    row_totals = [sum(row) for row in table]
+    col_totals = [sum(col) for col in zip(*table)]
+    total = sum(table[0]) + sum(table[1])
+    expected = [[row_totals[i] * col_totals[j] / total for j in range(2)] for i in range(2)]
+    chisq, pval = stats.chisquare([table[0][0], table[1][0]], f_exp=[expected[0][0], expected[1][0]])
     alpha = 0.07
-    count = np.array([x_success, y_success])
-    nobs = np.array([x_cnt, y_cnt])
-    p_val = proportions_ztest(count, nobs, alternative='larger')[1]
-    return True if p_val <= alpha else False # Ваш ответ, True или False
+    return pval < alpha
